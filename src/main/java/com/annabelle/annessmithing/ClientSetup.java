aa61938dfe8ee688924f51847f1b8890b5293f3b
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-05-29 16:39:45 -0700

    Added Sword model

--- src/main/java/com/annabelle/annessmithing/ClientSetup.java
+++ src/main/java/com/annabelle/annessmithing/ClientSetup.java
@@ -1,12 +1,15 @@
 package com.annabelle.annessmithing;
 
 import com.annabelle.annessmithing.item.ModItems;
+import com.annabelle.annessmithing.item.custom.CustomSwordItem;
 import com.annabelle.annessmithing.itemmodels.CustomToolModel;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 
 public class ClientSetup {
     public static void registerItemColors(final ColorHandlerEvent.Item event) {
-        event.getItemColors().register(new CustomToolModel(), ModItems.BLANK_PICK.get());
-        event.getItemColors().register(new CustomToolModel(), ModItems.CUSTOM_HOE.get());
+        CustomToolModel toolModel = new CustomToolModel();
+        event.getItemColors().register(toolModel, ModItems.BLANK_PICK.get());
+        event.getItemColors().register(toolModel, ModItems.CUSTOM_HOE.get());
+        event.getItemColors().register(toolModel, ModItems.CUSTOM_SWORD.get());
     }
 }
