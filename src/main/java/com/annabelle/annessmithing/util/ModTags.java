8518bc24bfb86f241386cdeaa2d2f1af7a438b17
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-05-29 15:32:10 -0700

    Added sword_head tag, added flint sword head to tag

--- src/main/java/com/annabelle/annessmithing/util/ModTags.java
+++ src/main/java/com/annabelle/annessmithing/util/ModTags.java
@@ -10,8 +10,8 @@ import net.minecraftforge.common.Tags;
 public class ModTags {
     public static class Items {
         public static final TagKey<Item> PICKAXE_HEADS = tag("tool_components/pickaxe_head");
-
         public static final TagKey<Item> HOE_HEADS = tag("tool_components/hoe_head");
+        public static final TagKey<Item> SWORD_HEADS = tag("tool_components/sword_head");
         public static final TagKey<Item> TOOL_RODS = tag("tool_components/tool_rod");
         public static final TagKey<Item> TOOL_BINDERS = tag("tool_components/tool_binder");
 
