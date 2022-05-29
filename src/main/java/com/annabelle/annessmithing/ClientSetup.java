2489f46d69a33c61d97ac189ae9945be0b3460d5
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-05-28 18:46:00 -0700

    Added Hoe model

--- src/main/java/com/annabelle/annessmithing/ClientSetup.java
+++ src/main/java/com/annabelle/annessmithing/ClientSetup.java
@@ -7,5 +7,6 @@ import net.minecraftforge.client.event.ColorHandlerEvent;
 public class ClientSetup {
     public static void registerItemColors(final ColorHandlerEvent.Item event) {
         event.getItemColors().register(new CustomToolModel(), ModItems.BLANK_PICK.get());
+        event.getItemColors().register(new CustomToolModel(), ModItems.CUSTOM_HOE.get());
     }
 }
