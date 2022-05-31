fdce51188c16589b002d7f12e97da5347e820887
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-05-31 14:02:25 -0700

    Generated Models for Copper and Flint Part heads

---
+++ src/main/java/com/annabelle/annessmithing/datagen/DataGenerators.java
@@ -0,0 +1,20 @@
+package com.annabelle.annessmithing.datagen;
+
+import com.annabelle.annessmithing.AnnesSmithing;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = AnnesSmithing.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+
+        generator.addProvider(new ModItemModelProvider(generator, existingFileHelper));
+    }
+}
