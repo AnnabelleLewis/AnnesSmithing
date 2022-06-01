f9135d5bb06f52a02304789c567602deefa8b4f0
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-06-01 14:28:55 -0700

    Created Repair recipie type

---
+++ src/main/java/com/annabelle/annessmithing/recipie/ToolRepairRecipe.java
@@ -0,0 +1,86 @@
+package com.annabelle.annessmithing.recipie;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import net.minecraft.core.NonNullList;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import net.minecraft.world.item.crafting.ShapelessRecipe;
+
+public class ToolRepairRecipe extends ShapelessRecipe {
+
+    private final ResourceLocation id;
+    final String group;
+    final ItemStack result;
+    final NonNullList<Ingredient> ingredients;
+
+    public ToolRepairRecipe(ResourceLocation pId, String pGroup, ItemStack pResult, NonNullList<Ingredient> pIngredients) {
+        super(pId, pGroup, pResult, pIngredients);
+        this.id = pId;
+        this.group = pGroup;
+        this.result = pResult;
+        this.ingredients = pIngredients;
+    }
+
+    public ItemStack assemble(CraftingContainer pInv) {
+        return this.result.copy();
+    }
+
+    public static class Serializer extends net.minecraftforge.registries.ForgeRegistryEntry<RecipeSerializer<?>> implements RecipeSerializer<ToolRepairRecipe> {
+        private static final ResourceLocation NAME = new ResourceLocation("minecraft", "crafting_tool_repair");
+        public ToolRepairRecipe fromJson(ResourceLocation pRecipeId, JsonObject pJson) {
+            String s = GsonHelper.getAsString(pJson, "group", "");
+            NonNullList<Ingredient> nonnulllist = itemsFromJson(GsonHelper.getAsJsonArray(pJson, "ingredients"));
+            if (nonnulllist.isEmpty()) {
+                throw new JsonParseException("No ingredients for shapeless recipe");
+            } else  {
+                ItemStack itemstack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pJson, "result"));
+                return new ToolRepairRecipe(pRecipeId, s, itemstack, nonnulllist);
+            }
+        }
+
+        private static NonNullList<Ingredient> itemsFromJson(JsonArray pIngredientArray) {
+            NonNullList<Ingredient> nonnulllist = NonNullList.create();
+
+            for(int i = 0; i < pIngredientArray.size(); ++i) {
+                Ingredient ingredient = Ingredient.fromJson(pIngredientArray.get(i));
+                if (net.minecraftforge.common.ForgeConfig.SERVER.skipEmptyShapelessCheck.get() || !ingredient.isEmpty()) {
+                    nonnulllist.add(ingredient);
+                }
+            }
+
+            return nonnulllist;
+        }
+
+        public ToolRepairRecipe fromNetwork(ResourceLocation pRecipeId, FriendlyByteBuf pBuffer) {
+            String s = pBuffer.readUtf();
+            int i = pBuffer.readVarInt();
+            NonNullList<Ingredient> nonnulllist = NonNullList.withSize(i, Ingredient.EMPTY);
+
+            for(int j = 0; j < nonnulllist.size(); ++j) {
+                nonnulllist.set(j, Ingredient.fromNetwork(pBuffer));
+            }
+
+            ItemStack itemstack = pBuffer.readItem();
+            return new ToolRepairRecipe(pRecipeId, s, itemstack, nonnulllist);
+        }
+
+        public void toNetwork(FriendlyByteBuf pBuffer, ToolRepairRecipe pRecipe) {
+            pBuffer.writeUtf(pRecipe.group);
+            pBuffer.writeVarInt(pRecipe.ingredients.size());
+
+            for(Ingredient ingredient : pRecipe.ingredients) {
+                ingredient.toNetwork(pBuffer);
+            }
+
+            pBuffer.writeItem(pRecipe.result);
+        }
+    }
+}
